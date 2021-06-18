662ae10a4ebb4f4da4fdfb8cc516f100c991e30e
BatmanofZuhandArrgh <anhnguyen199819981998@gmail.com>
2021-06-19 00:02:20 +0700

    Add grid to game

--- zone.py
+++ zone.py
@@ -1,46 +1,92 @@
 import os
+import sys
+import numpy as np
 import pygame
+from pygame.locals import *
+
+WHITE = (255, 255, 255)
+NAVY_BLUE = (0,0,128)
+BLACK = (0, 0, 0)
 
 class Surface:
     def __init__(self, 
-        width, 
-        height,
+        width: int = 1000,
+        divider:int = 20,
         ):
-
-        self.size = (height, width)
+        
+        if(width % divider != 0):
+            width -= width%divider
+        
+        self.width = width
+        self.size = (width, width)
         self.empty_surface = None
+        self.screen = None
+
+        self.blockSize = int(width // divider)
+
+        self.screen_representation = np.zeros(((self.blockSize, self.blockSize)))
+        self.screen_full = np.zeros((self.size))
 
     def show_surface(self):
         self.empty_surface = pygame.Surface(self.size)
         return self.empty_surface
 
+    def drawGrid(self):
+        for x in range(0, self.width, self.blockSize):
+            for y in range(0, self.width, self.blockSize):
+                rect = pygame.Rect(x, y, self.blockSize, self.blockSize)
+                pygame.draw.rect(self.screen, WHITE, rect, 1)
+    
+    def paintGridBox(self, mouse_position):
+        rep_width_index, rep_height_index = mouse_position[0]//self.blockSize, mouse_position[1]//self.blockSize
+
+        # self.screen_representation[rep_width_index][rep_height_index] = 1
+        
+        for x in range(rep_width_index*self.blockSize, (rep_width_index+1)*self.blockSize):
+            for y in range(rep_height_index*self.blockSize, (rep_height_index+1)*self.blockSize):
+                pygame.draw.circle(self.screen, WHITE, (x,y), 1)
+
     def game_init(self):
         pygame.init()
-        screen = pygame.display.set_mode(self.size)
+
+        mouse_position = (0, 0)
+        drawing = False
+
+        self.screen = pygame.display.set_mode(self.size)
         done = False
-        screen.fill((255,255,255))
+        self.screen.fill(BLACK)
 
+        last_pos = None
         while not done:
+            self.drawGrid()            
             for event in pygame.event.get():
-                if(event.type == pygame.KEYDOWN):
-                    if event.key == pygame.K_q:
-                        done = True
-                    elif event.key == pygame.K_a:
-                        print('Move left')
-                    elif event.key == pygame.K_w:
-                        print('Move up')
-                    elif event.key == pygame.K_d:
-                        print('Move right')
-                    elif event.key == pygame.K_s:
-                        print('Move down')
+                if event.type == QUIT:
+                    pygame.quit()
+                    sys.exit()
+                elif event.type == MOUSEMOTION:
+                    if (drawing): 
+                        mouse_position = pygame.mouse.get_pos()
+                        self.paintGridBox(mouse_position)
 
-            screen.blit(self.show_surface(), self.size)
+                elif event.type == MOUSEBUTTONUP:
+                    mouse_position = (0, 0)
+                    drawing = False
+
+                elif event.type == MOUSEBUTTONDOWN:
+                    drawing = True
+                    mouse_position = pygame.mouse.get_pos()
+                    self.paintGridBox(mouse_position)
+
+                elif event.type == KEYDOWN:
+                    if event.key == pygame.K_q:
+                        pygame.quit()
+                        sys.exit()    
+
+            pygame.display.update()
+
 
 
 if __name__ == "__main__":
-    surface = Surface(
-        width = 1000,
-        height= 1000,
-    )
+    surface = Surface()
 
     surface.game_init()
