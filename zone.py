c708b776c69e6ae9e1a2056468ddd6c0d61aa6ab
BatmanofZuhandArrgh <anhnguyen199819981998@gmail.com>
2021-06-20 23:05:32 +0700

    Added game creation and game winning condition, player icon and mechanism

--- zone.py
+++ zone.py
@@ -2,16 +2,49 @@ import os
 import sys
 import numpy as np
 import pygame
+from pygame import mouse
 from pygame.locals import *
 
 WHITE = (255, 255, 255)
-NAVY_BLUE = (0,0,128)
 BLACK = (0, 0, 0)
 
+class test_block:
+    color = WHITE
+    index = 1
+
+class lava:
+    #Slowly hurts player if walked on
+    color = (180,41,33)
+    index = 1
+
+class water: 
+    #Severely hurts player if walked on
+    color = (0,0,205)
+    index = 2
+
+class escape_door:
+    #Finish game
+    color = (0,220,0)
+    index = 3
+
+INDEX_DICT = {
+    1: lava,
+    2: water,
+    3: escape_door,
+}
+
 class Surface:
+    TEXT_PHASE = [
+        "Designing phase:   Press q to quit, spacebar to continue",
+        "Locating lava",
+        "Locating water",
+        "Locating escape door",
+    ]
+
     def __init__(self, 
         width: int = 1000,
         divider:int = 20,
+        icon_image_path:str = 'assets/icons8-batman-48.png', 
         ):
         
         if(width % divider != 0):
@@ -26,6 +59,10 @@ class Surface:
 
         self.screen_representation = np.zeros(((self.blockSize, self.blockSize)))
         self.screen_full = np.zeros((self.size))
+        
+        #Player position
+        self.icon = pygame.image.load(icon_image_path)
+        self.position = (0,0)
 
     def show_surface(self):
         self.empty_surface = pygame.Surface(self.size)
@@ -36,54 +73,100 @@ class Surface:
             for y in range(0, self.width, self.blockSize):
                 rect = pygame.Rect(x, y, self.blockSize, self.blockSize)
                 pygame.draw.rect(self.screen, WHITE, rect, 1)
+            
+        for i in range(len(self.screen_representation)):
+            for j in range(len(self.screen_representation)):
+                if self.screen_representation[i][j] != 0:
+                    self.paintGridBox(i, j, INDEX_DICT[self.screen_representation[i][j]])
     
-    def paintGridBox(self, mouse_position):
+    def changeScreenRep(self, mouse_position, cell_type):
+        #Changing a specific index number in the screen representation, to signal the gridbox to change color
         rep_width_index, rep_height_index = mouse_position[0]//self.blockSize, mouse_position[1]//self.blockSize
 
-        # self.screen_representation[rep_width_index][rep_height_index] = 1
+        self.screen_representation[rep_width_index][rep_height_index] = cell_type.index
+            
+    def paintGridBox(self, rep_width_index, rep_height_index, cell_type):
+        #Painting inside gridbox, either lava, spikes or exit_door
         
         for x in range(rep_width_index*self.blockSize, (rep_width_index+1)*self.blockSize):
             for y in range(rep_height_index*self.blockSize, (rep_height_index+1)*self.blockSize):
-                pygame.draw.circle(self.screen, WHITE, (x,y), 1)
+                pygame.draw.circle(self.screen, cell_type.color, (x,y), 1)
+
+    def check_win_condition(self):
+        if(3 not in self.screen_representation):
+            return False
+        
+        if(self.screen_representation[self.position[0], self.position[1]] == 3):
+            return True
 
     def game_init(self):
         pygame.init()
 
-        mouse_position = (0, 0)
+        mouse_position = (-1, -1)
         drawing = False
 
         self.screen = pygame.display.set_mode(self.size)
+        font = pygame.font.SysFont('Comic Sans MS', 30)
+
         done = False
         self.screen.fill(BLACK)
+        
+        text = self.TEXT_PHASE[0]
+        text_surface = font.render(text, True, WHITE)
+        self.screen.blit(text_surface, dest=(0,0))
 
-        last_pos = None
+        cell_type = lava
+
         while not done:
-            self.drawGrid()            
             for event in pygame.event.get():
-                if event.type == QUIT:
-                    pygame.quit()
-                    sys.exit()
-                elif event.type == MOUSEMOTION:
+                if event.type == MOUSEMOTION:
                     if (drawing): 
                         mouse_position = pygame.mouse.get_pos()
-                        self.paintGridBox(mouse_position)
+                        self.changeScreenRep(mouse_position, cell_type)
 
                 elif event.type == MOUSEBUTTONUP:
-                    mouse_position = (0, 0)
+                    mouse_position = (-1, -1)
                     drawing = False
 
                 elif event.type == MOUSEBUTTONDOWN:
                     drawing = True
                     mouse_position = pygame.mouse.get_pos()
-                    self.paintGridBox(mouse_position)
+                    self.changeScreenRep(mouse_position, cell_type)
 
                 elif event.type == KEYDOWN:
                     if event.key == pygame.K_q:
                         pygame.quit()
                         sys.exit()    
+                    elif event.key == pygame.K_SPACE and text != self.TEXT_PHASE[-1] and text in self.TEXT_PHASE:
+                        text = self.TEXT_PHASE[self.TEXT_PHASE.index(text)+1]
+                        text_surface = font.render(text, True, WHITE)
+                        cell_type = INDEX_DICT[self.TEXT_PHASE.index(text)]
+                    elif text in (self.TEXT_PHASE[-1], ''):
+                        mouse_position = (-1,-1)
+                        if(event.key == pygame.K_UP):
+                            self.position = (self.position[0], max(0, self.position[1]-1))
+                        elif(event.key == pygame.K_DOWN):
+                            self.position = (self.position[0], min(self.position[1] + 1, self.width))
+                        elif(event.key == pygame.K_LEFT):
+                            self.position = (max(self.position[0] - 1, 0), self.position[1])
+                        elif(event.key == pygame.K_RIGHT):
+                            self.position = (min(self.position[0] + 1, self.width), self.position[1])
+
+                        text = ''
+                        text_surface = font.render(text, True, WHITE)
+                        
+
+            self.screen.fill(BLACK)
+            self.drawGrid()        
+
+            if(self.check_win_condition()):
+                text = 'You won'
+                text_surface = font.render(text, True, (0,255,0))
+             
+            self.screen.blit(self.icon, (self.position[0] * self.blockSize, self.position[1] *  self.blockSize))
+            self.screen.blit(text_surface, (0,0))
 
             pygame.display.update()
-
 
 
 if __name__ == "__main__":
